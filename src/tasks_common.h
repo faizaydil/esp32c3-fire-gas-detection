508f0c0312106fa10f06b65eca2fc66c438557fa
faizaydil <faizaydil@gmail.com>
2025-12-17 21:59:35 +0800

    Update tasks_common.h

--- src/tasks_common.h
+++ src/tasks_common.h
@@ -2,41 +2,35 @@
 #define TASKS_COMMON_H
 
 #include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/message_buffer.h"
 #include "freertos/event_groups.h"
+#include "freertos/message_buffer.h"
 #include "freertos/semphr.h"
-#include "driver/gpio.h"
-#include "esp_log.h"
 
-// ---- GPIO MAP ----
-#define MQ2_DO_GPIO        GPIO_NUM_6
-#define MQ135_DO_GPIO      GPIO_NUM_7
-#define BUZZER_GPIO        GPIO_NUM_5
-#define LED_RED_GPIO       GPIO_NUM_3
-#define LED_GREEN_GPIO     GPIO_NUM_4
-#define LED_YELLOW_GPIO    GPIO_NUM_2
-#define OLED_SDA_GPIO      GPIO_NUM_9
-#define OLED_SCL_GPIO      GPIO_NUM_10
+/* GPIO Mapping */
+#define DHT11_GPIO        8
+#define BUZZER_GPIO       5
+#define LED_RED_GPIO      3
+#define LED_GREEN_GPIO    4
+#define LED_YELLOW_GPIO   2
+#define BTN_HIGH_TEMP     0
+#define BTN_LOW_HUM       1
 
-// Message buffer size
-#define MSG_BUF_SIZE       128
+/* Fire Thresholds */
+#define TEMP_THRESHOLD    35
+#define HUM_THRESHOLD     30
 
-// Event bits
-#define BIT_MQ2_ALARM      (1 << 0)
-#define BIT_MQ135_ALARM    (1 << 1)
+/* Event Group Bits */
+#define EVT_FIRE_RISK     (1 << 0)
 
-// Struct for sensor data
+/* Shared Handles */
+extern EventGroupHandle_t fire_event_group;
+extern MessageBufferHandle_t sensor_msg_buffer;
+extern SemaphoreHandle_t oled_mutex;
+
+/* Sensor Data Structure */
 typedef struct {
-    uint32_t ts;
-    bool mq2_do;
-    bool mq135_do;
-} SensorMsg_t;
-
-// Global handles
-extern MessageBufferHandle_t xSensorMsgBuf;
-extern EventGroupHandle_t xEventGroup;
-extern SemaphoreHandle_t xResourceMutex;
-extern TaskHandle_t xEmergencyTaskHandle;
+    int temperature;
+    int humidity;
+} dht_data_t;
 
 #endif
