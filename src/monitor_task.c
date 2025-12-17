26a015358d21dd19155b84d13bd51154fb92fcda
faizaydil <faizaydil@gmail.com>
2025-12-17 22:45:10 +0800

    Update monitor_task.c

--- src/monitor_task.c
+++ src/monitor_task.c
@@ -7,7 +7,8 @@ void monitor_task(void *pv)
 
     while (1) {
         if (xMessageBufferReceive(sensor_msg_buffer, &data, sizeof(data), portMAX_DELAY)) {
-            if (data.temperature > TEMP_THRESHOLD && data.humidity < HUM_THRESHOLD) {
+            if (data.temperature > TEMP_THRESHOLD &&
+                data.humidity < HUM_THRESHOLD) {
                 xEventGroupSetBits(fire_event_group, EVT_FIRE_RISK);
             }
         }
