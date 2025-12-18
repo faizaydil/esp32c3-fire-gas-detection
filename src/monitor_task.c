2504b469332224a9a33279d343b49746e4d13e4f
faizaydil <faizaydil@gmail.com>
2025-12-18 09:15:38 +0800

    Update monitor_task.c

--- src/monitor_task.c
+++ src/monitor_task.c
@@ -1,15 +1,20 @@
-#include "tasks_common.h"
+#include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "freertos/message_buffer.h"
+#include "tasks_common.h"
+
+extern EventGroupHandle_t fire_event_group;
+extern MessageBufferHandle_t sensor_msg_buffer;
 
 void monitor_task(void *pv)
 {
     dht_data_t data;
 
     while (1) {
-        if (xMessageBufferReceive(sensor_msg_buffer, &data, sizeof(data), portMAX_DELAY)) {
-            if (data.temperature > TEMP_THRESHOLD &&
-                data.humidity < HUM_THRESHOLD) {
-                xEventGroupSetBits(fire_event_group, EVT_FIRE_RISK);
+        if (xMessageBufferReceive(sensor_msg_buffer, &data, sizeof(data), pdMS_TO_TICKS(1000))) {
+            if (data.temperature > 40.0) {
+                xEventGroupSetBits(fire_event_group, TEMP_EVENT_BIT);
             }
         }
     }
