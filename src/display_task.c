76f357164723244925f4b3b5d38adb75d29252c7
faizaydil <faizaydil@gmail.com>
2025-12-18 13:43:10 +0800

    Update display_task.c

--- src/display_task.c
+++ src/display_task.c
@@ -1,26 +1,46 @@
+#include <stdio.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/message_buffer.h"
+#include "freertos/event_groups.h"
 #include "freertos/semphr.h"
-#include "lcd1602_driver.h"
+
 #include "tasks_common.h"
+#include "lcd1602_driver.h"
 
 extern MessageBufferHandle_t sensor_msg_buffer;
+extern EventGroupHandle_t fire_event_group;
 extern SemaphoreHandle_t lcd_mutex;
 
 void display_task(void *pv)
 {
     dht_data_t data;
-    char line[16];
+    char line1[17];
+    char line2[17];
 
     while (1) {
-        if (xMessageBufferReceive(sensor_msg_buffer, &data, sizeof(data), portMAX_DELAY)) {
+        if (xMessageBufferReceive(sensor_msg_buffer,
+                                  &data,
+                                  sizeof(data),
+                                  portMAX_DELAY) == sizeof(data)) {
+
             if (xSemaphoreTake(lcd_mutex, portMAX_DELAY)) {
+
+                snprintf(line1, 17, "T:%.1fC H:%.1f%%",
+                         data.temperature, data.humidity);
+
+                if (xEventGroupGetBits(fire_event_group) & EVT_FIRE_RISK)
+                    snprintf(line2, 17, "!! FIRE RISK !!");
+                else
+                    snprintf(line2, 17, "Status: NORMAL");
+
                 lcd_clear();
-                snprintf(line, 16, "Temp: %.1fC", data.temperature);
-                lcd_print(0, 0, line);
-                snprintf(line, 16, "Hum: %.1f%%", data.humidity);
-                lcd_print(1, 0, line);
+                lcd_set_cursor(0, 0);
+                lcd_print(line1);
+                lcd_set_cursor(0, 1);
+                lcd_print(line2);
+
                 xSemaphoreGive(lcd_mutex);
             }
         }
