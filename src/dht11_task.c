5c5cdc77efb2775b5eb1a01ad592b4bfa523d612
faizaydil <faizaydil@gmail.com>
2025-12-18 09:14:19 +0800

    Update dht11_task.c

--- src/dht11_task.c
+++ src/dht11_task.c
@@ -1,20 +1,18 @@
-#include "tasks_common.h"
+#include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_random.h"
+#include "freertos/message_buffer.h"
+#include "tasks_common.h"
 
-/* Simulated DHT11 Read (acceptable for coursework) */
-static void read_dht11(dht_data_t *data)
-{
-    data->temperature = 25 + (esp_random() % 20);
-    data->humidity = 30 + (esp_random() % 40);
-}
+extern MessageBufferHandle_t sensor_msg_buffer;
 
 void dht11_task(void *pv)
 {
     dht_data_t data;
 
     while (1) {
-        read_dht11(&data);
+        data.temperature = 35.0;   // simulated
+        data.humidity = 60.0;
+
         xMessageBufferSend(sensor_msg_buffer, &data, sizeof(data), 0);
         vTaskDelay(pdMS_TO_TICKS(2000));
     }
