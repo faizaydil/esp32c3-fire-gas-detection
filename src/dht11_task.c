f5cd6fc93ee7f7f11f55d6e62130c95ec442dafd
faizaydil <faizaydil@gmail.com>
2025-12-18 13:38:32 +0800

    Update dht11_task.c

--- src/dht11_task.c
+++ src/dht11_task.c
@@ -1,6 +1,6 @@
+#include <stdlib.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/message_buffer.h"
 #include "tasks_common.h"
 
 extern MessageBufferHandle_t sensor_msg_buffer;
@@ -10,8 +10,8 @@ void dht11_task(void *pv)
     dht_data_t data;
 
     while (1) {
-        data.temperature = 35.0;   // simulated
-        data.humidity = 60.0;
+        data.temperature = 20 + (rand() % 30);   // 20–49 °C
+        data.humidity = 40 + (rand() % 40);      // 40–79 %
 
         xMessageBufferSend(sensor_msg_buffer, &data, sizeof(data), 0);
         vTaskDelay(pdMS_TO_TICKS(2000));
