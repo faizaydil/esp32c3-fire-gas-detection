941015ecfb76839e11487de40edd934cc1f6f4ce
faizaydil <faizaydil@gmail.com>
2025-12-18 09:13:58 +0800

    Update app_main.c

--- src/app_main.c
+++ src/app_main.c
@@ -1,14 +1,20 @@
-#include "tasks_common.h"
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "freertos/message_buffer.h"
+#include "freertos/semphr.h"
 #include "driver/gpio.h"
+
+#include "tasks_common.h"
 #include "lcd1602_driver.h"
 
-/* RTOS Objects */
+/* RTOS objects */
 EventGroupHandle_t fire_event_group;
 MessageBufferHandle_t sensor_msg_buffer;
 SemaphoreHandle_t lcd_mutex;
 
-/* Task Prototypes */
+/* Task prototypes */
 void dht11_task(void *pv);
 void monitor_task(void *pv);
 void emergency_task(void *pv);
@@ -29,6 +35,6 @@ void app_main(void)
 
     xTaskCreate(dht11_task, "DHT11 Task", 4096, NULL, 2, NULL);
     xTaskCreate(monitor_task, "Monitor Task", 4096, NULL, 3, NULL);
-    xTaskCreate(emergency_task, "Emergency Task", 4096, NULL, 5, NULL);
+    xTaskCreate(emergency_task, "Emergency Task", 4096, NULL, 4, NULL);
     xTaskCreate(display_task, "Display Task", 4096, NULL, 1, NULL);
 }
