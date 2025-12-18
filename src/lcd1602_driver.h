f3229161a4c550c9a6a7f5376f7c514687bdb46e
faizaydil <faizaydil@gmail.com>
2025-12-18 09:15:26 +0800

    Update lcd1602_driver.h

--- src/lcd1602_driver.h
+++ src/lcd1602_driver.h
@@ -1,7 +1,5 @@
-#ifndef LCD1602_DRIVER_H
-#define LCD1602_DRIVER_H
+#pragma once
 
 void lcd_init(void);
-void lcd_show_status(int temp, int hum, const char *status);
-
-#endif
+void lcd_clear(void);
+void lcd_print(int row, int col, const char *text);
