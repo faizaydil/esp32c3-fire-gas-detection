1be5bbf3735fbf2621d18b4a51bfa8b6c68cd813
faizaydil <faizaydil@gmail.com>
2025-12-18 09:14:33 +0800

    Update display_task.c

--- src/display_task.c
+++ src/display_task.c
@@ -1,28 +1,28 @@
-#include "tasks_common.h"
+#include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/message_buffer.h"
+#include "freertos/semphr.h"
 #include "lcd1602_driver.h"
+#include "tasks_common.h"
+
+extern MessageBufferHandle_t sensor_msg_buffer;
+extern SemaphoreHandle_t lcd_mutex;
 
 void display_task(void *pv)
 {
     dht_data_t data;
+    char line[16];
 
     while (1) {
-        if (xMessageBufferReceive(sensor_msg_buffer, &data, sizeof(data), 0)) {
-
+        if (xMessageBufferReceive(sensor_msg_buffer, &data, sizeof(data), portMAX_DELAY)) {
             if (xSemaphoreTake(lcd_mutex, portMAX_DELAY)) {
-
-                if (data.temperature > TEMP_THRESHOLD &&
-                    data.humidity < HUM_THRESHOLD) {
-                    lcd_show_status(data.temperature, data.humidity, "FIRE RISK");
-                    gpio_set_level(LED_GREEN_GPIO, 0);
-                } else {
-                    lcd_show_status(data.temperature, data.humidity, "NORMAL");
-                    gpio_set_level(LED_GREEN_GPIO, 1);
-                }
-
+                lcd_clear();
+                snprintf(line, 16, "Temp: %.1fC", data.temperature);
+                lcd_print(0, 0, line);
+                snprintf(line, 16, "Hum: %.1f%%", data.humidity);
+                lcd_print(1, 0, line);
                 xSemaphoreGive(lcd_mutex);
             }
         }
-        vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
