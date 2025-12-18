b3e2c45261d4320af3cd382cb799ea1fde6365f5
faizaydil <faizaydil@gmail.com>
2025-12-18 13:37:36 +0800

    Update app_main.c

--- src/app_main.c
+++ src/app_main.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
 #include "freertos/message_buffer.h"
 #include "freertos/semphr.h"
-#include "driver/gpio.h"
 
 #include "tasks_common.h"
 #include "lcd1602_driver.h"
 
-/* RTOS objects */
+/* RTOS Objects */
 EventGroupHandle_t fire_event_group;
 MessageBufferHandle_t sensor_msg_buffer;
 SemaphoreHandle_t lcd_mutex;
@@ -22,19 +20,14 @@ void display_task(void *pv);
 
 void app_main(void)
 {
-    fire_event_group = xEventGroupCreate();
-    sensor_msg_buffer = xMessageBufferCreate(sizeof(dht_data_t) * 4);
-    lcd_mutex = xSemaphoreCreateMutex();
-
-    gpio_set_direction(LED_RED_GPIO, GPIO_MODE_OUTPUT);
-    gpio_set_direction(LED_GREEN_GPIO, GPIO_MODE_OUTPUT);
-    gpio_set_direction(LED_YELLOW_GPIO, GPIO_MODE_OUTPUT);
-    gpio_set_direction(BUZZER_GPIO, GPIO_MODE_OUTPUT);
-
     lcd_init();
 
-    xTaskCreate(dht11_task, "DHT11 Task", 4096, NULL, 2, NULL);
-    xTaskCreate(monitor_task, "Monitor Task", 4096, NULL, 3, NULL);
-    xTaskCreate(emergency_task, "Emergency Task", 4096, NULL, 4, NULL);
-    xTaskCreate(display_task, "Display Task", 4096, NULL, 1, NULL);
+    fire_event_group = xEventGroupCreate();
+    sensor_msg_buffer = xMessageBufferCreate(sizeof(dht_data_t));
+    lcd_mutex = xSemaphoreCreateMutex();
+
+    xTaskCreate(dht11_task, "DHT11", 4096, NULL, 3, NULL);
+    xTaskCreate(monitor_task, "Monitor", 4096, NULL, 3, NULL);
+    xTaskCreate(emergency_task, "Emergency", 4096, NULL, 4, NULL);
+    xTaskCreate(display_task, "Display", 4096, NULL, 2, NULL);
 }
